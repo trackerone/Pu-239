9c257972a6a732bed99a51c5bc632ac01354e4f0
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-22 20:14:06 +0200

    Add files via upload

--- forums/delete_subscription.php
+++ forums/delete_subscription.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/runtime_safe.php';
+require_once __DIR__ . '/../include/runtime_safe.php';
+require_once __DIR__ . '/../include/mysql_compat.php';
 
 
 declare(strict_types = 1);
