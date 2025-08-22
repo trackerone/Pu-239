f8f9f8531ea87695efd4a2513da703d58f244671
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-22 21:30:56 +0200

    Add files via upload

--- cleanup/gift_update.php
+++ cleanup/gift_update.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__ . '/../include/runtime_safe.php';
-require_once __DIR__ . '/../include/mysql_compat.php';
+require_once __DIR__ . '/bootstrap_pdo.php';
 
 
 declare(strict_types = 1);
@@ -26,7 +25,7 @@ function gift_update($data)
 
     $time_start = microtime(true);
     if (Christmas()) {
-        app_halt();
+        die();
     }
     $fluent = $container->get(Database::class);
     $query = $fluent->from('users')
