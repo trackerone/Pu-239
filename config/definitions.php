0b58a5ad9b545b678b815de6b1d738788552c7ac
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-22 21:31:39 +0200

    Add files via upload

--- config/definitions.php
+++ config/definitions.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__ . '/../include/runtime_safe.php';
-require_once __DIR__ . '/../include/mysql_compat.php';
+require_once __DIR__ . '/bootstrap_pdo.php';
 
 
 declare(strict_types = 1);
@@ -35,7 +34,7 @@ return [
             $mysqli = new mysqli($env['db']['host'], $env['db']['username'], $env['db']['password'], $env['db']['database'], $env['db']['port']);
         }
         if ($mysqli->connect_error) {
-            app_halt('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+            die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
         }
 
         return $mysqli;
