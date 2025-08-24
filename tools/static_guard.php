206f525b6176069e92fcd334018f7efa2c497312
trackerone <51250404+trackerone@users.noreply.github.com>
2025-08-24 09:09:50 +0200

    Update static_guard.php

--- tools/static_guard.php
+++ tools/static_guard.php
@@ -22,18 +22,18 @@ $maxErrors = ($maxErrors !== false && $maxErrors !== '') ? intval($maxErrors) : 
 
 $patterns = [
     'merge_conflict_marker' => '/^(<<<<<<<|=======|>>>>>>>)/m',
-    'short_open_tag'        => '/<\?(?!php|=)/',
-    'eval_usage'            => '/\beval\s*\(/i',
-    'terminate_calls'       => '/\b(die|exit)\s*\(/i',
-    'debug_calls'           => '/\b(var_dump|print_r|dd)\s*\(/i',
-    'deprecated_mysql'      => '/\bmysql_(query|connect|pconnect|select_db|fetch_(assoc|array|row)|num_rows|real_escape_string|insert_id|error|errno)\b/i',
+    'short_open_tag'        => '/<\\?(?!php|=)/',
+    'eval_usage'            => '/\\beval\\s*\\(/i',
+    'terminate_calls'       => '/\\b(die|exit)\\s*\\(/i',
+    'debug_calls'           => '/\\b(var_dump|print_r|dd)\\s*\\(/i',
+    'deprecated_mysql'      => '/\\bmysql_(query|connect|pconnect|select_db|fetch_(assoc|array|row)|num_rows|real_escape_string|insert_id|error|errno)\\b/i',
 ];
 
 $issues = [];
 foreach ($rii as $file) {
     if ($file->isDir()) continue;
     $path = $file->getPathname();
-    if (!preg_match('/\.php$/i', $path)) continue;
+    if (!preg_match('/\\.php$/i', $path)) continue;
 
     $rel = substr($path, strlen($root));
 
