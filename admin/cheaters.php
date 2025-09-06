dc488f46e7481e7c9317b426d9488f34edacfa42
trackerone <51250404+trackerone@users.noreply.github.com>
2025-09-06 16:16:03 +0200

    Update cheaters.php

--- admin/cheaters.php
+++ admin/cheaters.php
@@ -1,22 +1,26 @@
 <?php
-$db = $container->get(Database::class);
+declare(strict_types=1);
 
 require_once __DIR__ . '/../include/runtime_safe.php';
 
-require_once __DIR__ . '/../include/bootstrap_pdo.php';
-
-
-declare(strict_types = 1);
-
 use Pu239\Database;
+use Pu239\Cache;
 
 require_once INCL_DIR . 'function_users.php';
 require_once INCL_DIR . 'function_html.php';
 require_once INCL_DIR . 'function_pager.php';
 require_once CLASS_DIR . 'class_check.php';
+
 $class = get_access(basename($_SERVER['REQUEST_URI']));
 class_check($class);
-global $site_config;
+
+global $container, $site_config, $CURUSER;
+
+/** Script context: DB via container (no $this) */
+$db = $container->get(Database::class);
+
+/** Optional: cache for invalidations when disabling users */
+$cache = $container->get(Cache::class);
 
 $stdfoot = [
     'js' => [
@@ -24,30 +28,61 @@ $stdfoot = [
     ],
 ];
 
+$dt = TIME_NOW;
+
 if (isset($_POST['nowarned']) && $_POST['nowarned'] === 'nowarned') {
-    if (empty($_POST['desact']) && empty($_POST['remove'])) {
+    $ids_remove = !empty($_POST['remove']) ? array_map('intval', (array) $_POST['remove']) : [];
+    $ids_disable = !empty($_POST['desact']) ? array_map('intval', (array) $_POST['desact']) : [];
+
+    if (empty($ids_remove) && empty($ids_disable)) {
         stderr(_('Error'), _('You must select a user.'));
     }
-    if (!empty($_POST['remove'])) {
-        $db->run(');
+
+    // A) Remove selected cheater rows
+    if (!empty($ids_remove)) {
+        $in = implode(', ', $ids_remove);
+        $db->perform("DELETE FROM cheaters WHERE id IN ($in)");
     }
-    if (!empty($_POST['desact'])) {
-        $db->run(');
-        $this->cache->deleteMulti($_POST['desact']);
+
+    // B) Disable selected users (status=2) and prepend modcomment
+    if (!empty($ids_disable)) {
+        $in = implode(', ', $ids_disable);
+        $modline = get_date((int) $dt, 'DATE', 1) . ' - ' . _fe('Disabled by {0} (cheater panel)', $CURUSER['username']) . "\n";
+        $db->perform(
+            "UPDATE users
+                SET status = 2,
+                    modcomment = CONCAT(:modline, modcomment)
+              WHERE id IN ($in)",
+            ['modline' => $modline]
+        );
+        // Invalidate user cache rows
+        foreach ($ids_disable as $uid) {
+            $cache->delete('user_' . (int) $uid);
+        }
     }
+
+    header('Refresh: 2; url=' . htmlsafechars($_SERVER['REQUEST_URI']));
+    $changed = count($ids_remove) + count($ids_disable);
+    stderr(_('Success'), _pfe('{0} change applied.', '{0} changes applied.', $changed));
+    app_halt('Exit called');
 }
-$rows = $db->fetchAll('SELECT COUNT(id) FROM cheaters');
-$row = mysqli_fetch_array($res);
-$count = $row[0];
+
+/** Count + pagination */
+$count = (int) $db->fetchValue('SELECT COUNT(id) FROM cheaters');
 $perpage = 15;
+
 $HTMLOUT = "<h1 class='has-text-centered'>Possible Cheaters</h1>";
+
 if ($count > 0) {
     $pager = pager($perpage, $count, $site_config['paths']['baseurl'] . '/staffpanel.php?tool=cheaters&amp;action=cheaters&amp;');
+
     $HTMLOUT .= "
     <form action='{$_SERVER['PHP_SELF']}?tool=cheaters&amp;action=cheaters' method='post' enctype='multipart/form-data' accept-charset='utf-8'>";
+
     if ($count > $perpage) {
         $HTMLOUT .= $pager['pagertop'];
     }
+
     $heading = "
         <tr>
             <th class='w-1 has-text-centered'>#</th>
@@ -55,22 +90,31 @@ if ($count > 0) {
             <th class='w-1 has-text-centered'>" . _('Disable') . "</th>
             <th class='w-1 has-text-centered'>" . _('Remove') . '</th>
         </tr>';
-    $rows = $db->fetchAll('SELECT c.id AS cid, c.added, c.userid, c.torrentid, c.client, c.rate, c.beforeup, c.upthis, c.timediff, c.userip, t.id AS tid, t.name AS tname FROM cheaters AS c LEFT JOIN torrents AS t ON t.id=c.torrentid ORDER BY added DESC ' . $pager['limit']);
+
+    $rows = $db->fetchAll(
+        'SELECT c.id AS cid, c.added, c.userid, c.torrentid, c.client, c.rate, c.beforeup, c.upthis, c.timediff, c.userip,
+                t.id AS tid, t.name AS tname
+           FROM cheaters AS c
+      LEFT JOIN torrents AS t ON t.id = c.torrentid
+       ORDER BY c.added DESC ' . $pager['limit']
+    );
+
     $body = '';
     foreach ($rows as $arr) {
-        $id = $arr['cid'];
-        $userid = $arr['userid'];
+        $id       = (int) $arr['cid'];
+        $userid   = (int) $arr['userid'];
         $torrname = htmlsafechars(CutName($arr['tname'], 80));
-        $cheater = format_username((int) $userid) . ' ' . _(' has been flagged with an abnormally high upload speed!') . '<br>
-        ' . _('On torrent') . " <a href='{$site_config['paths']['baseurl']}/details.php?id=" . (int) $arr['tid'] . "' title='{$torrname}'>{$torrname}</a><br>
-        " . _('Uploaded') . ' ' . mksize((int) $arr['upthis']) . '<br>
-        ' . _('Speed') . ' ' . mksize((int) $arr['rate']) . '/s<br>
-        ' . _('Within') . ' ' . (int) $arr['timediff'] . ' ' . _('Seconds') . '<br>
-        ' . _('Using Client:') . ' ' . htmlsafechars($arr['client']) . '<br>
-        ' . _('Ip Address') . ' ' . htmlsafechars($arr['userip']);
+
+        $cheater = format_username($userid) . ' ' . _(' has been flagged with an abnormally high upload speed!') . '<br>'
+            . _('On torrent') . " <a href='{$site_config['paths']['baseurl']}/details.php?id=" . (int) $arr['tid'] . "' title='{$torrname}'>{$torrname}</a><br>"
+            . _('Uploaded') . ' ' . mksize((int) $arr['upthis']) . '<br>'
+            . _('Speed') . ' ' . mksize((int) $arr['rate']) . '/s<br>'
+            . _('Within') . ' ' . (int) $arr['timediff'] . ' ' . _('Seconds') . '<br>'
+            . _('Using Client:') . ' ' . htmlsafechars($arr['client']) . '<br>'
+            . _('Ip Address') . ' ' . htmlsafechars($arr['userip']);
 
         $cheaters = "
-        <div class='dt-tooltipper-large' data-tooltip-content='#cheater_{$id}_tooltip'>" . format_username((int) $userid, true, false) . "
+        <div class='dt-tooltipper-large' data-tooltip-content='#cheater_{$id}_tooltip'>" . format_username($userid, true, false) . "
             <div class='tooltip_templates'>
                 <div id='cheater_{$id}_tooltip'>$cheater</div>
             </div>
@@ -78,13 +122,15 @@ if ($count > 0) {
 
         $body .= "
         <tr>
-            <td class='has-text-centered'>{$arr['cid']}</td>
-            <td>$cheaters</td>
+            <td class='has-text-centered'>{$id}</td>
+            <td>{$cheaters}</td>
             <td class='has-text-centered'><input type='checkbox' name='desact[]' value='{$userid}'></td>
-            <td class='has-text-centered'><input type='checkbox' name='remove[]' value='{$arr['cid']}'></td>
+            <td class='has-text-centered'><input type='checkbox' name='remove[]' value='{$id}'></td>
         </tr>";
     }
+
     $HTMLOUT .= main_table($body, $heading);
+
     $HTMLOUT .= "
         <div class='has-text-centered margin20'>
             <input type='button' value='" . _('Check All Disable') . "' onclick=\"this.value=check1(this.form.elements['desact[]'])\" class='button is-small'>
@@ -93,15 +139,18 @@ if ($count > 0) {
             <input type='submit' name='submit' value='" . _('Apply Changes') . "' class='button is-small'>
         </div>
     </form>";
+
     if ($count > $perpage) {
         $HTMLOUT .= $pager['pagerbottom'];
     }
 } else {
     stderr(_('Error'), _('There are not any cheaters'), 'bottom20');
 }
+
 $title = _('Ratio Cheats');
 $breadcrumbs = [
     "<a href='{$site_config['paths']['baseurl']}/staffpanel.php'>" . _('Staff Panel') . '</a>',
     "<a href='{$_SERVER['PHP_SELF']}'>$title</a>",
 ];
+
 echo stdhead($title, [], 'page-wrapper', $breadcrumbs) . wrapper($HTMLOUT) . stdfoot();
