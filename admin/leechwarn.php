078d92fb4e49e6a8d2adc1a54e6fd8294167a58d
trackerone <51250404+trackerone@users.noreply.github.com>
2025-09-07 20:42:42 +0200

    Update leechwarn.php

--- admin/leechwarn.php
+++ admin/leechwarn.php
@@ -3,97 +3,102 @@ declare(strict_types=1);
 
 use Pu239\Database;
 use Pu239\Cache;
+use Pu239\Message;
 
 require_once __DIR__ . '/../include/runtime_safe.php';
+require_once __DIR__ . '/../include/bootstrap_pdo.php';
 require_once INCL_DIR . 'function_users.php';
 require_once INCL_DIR . 'function_html.php';
 require_once CLASS_DIR . 'class_check.php';
 
 global $container, $site_config, $CURUSER;
 
+/** @var Database $db */
 $db = $container->get(Database::class);
+/** @var Cache $cache */
+$cache = $container->get(Cache::class);
+/** @var Message $messages */
+$messages = $container->get(Message::class);
 
 $class = get_access(basename($_SERVER['REQUEST_URI']));
 class_check($class);
 
 $HTMLOUT = '';
-$this_url = $_SERVER['SCRIPT_NAME'];
-$do = isset($_GET['do']) && $_GET['do'] === 'disabled' ? 'disabled' : 'leechwarn';
+$this_url = $_SERVER['SCRIPT_NAME'] ?? '';
+$do = (isset($_GET['do']) && $_GET['do'] === 'disabled') ? 'disabled' : 'leechwarn';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $r = isset($_POST['ref']) ? $_POST['ref'] : $this_url;
-    $_uids = isset($_POST['users']) ? array_map('intval', $_POST['users']) : 0;
-    if ($_uids == 0 || count($_uids) == 0) {
+    $r = isset($_POST['ref']) ? (string) $_POST['ref'] : $this_url;
+    $uids = isset($_POST['users']) && is_array($_POST['users']) ? array_values(array_unique(array_map('intval', $_POST['users']))) : [];
+    if (empty($uids)) {
         stderr(_('Error'), _("Looks like you didn't select any user!"));
     }
-    $valid = [
-        'unwarn',
-        'disable',
-        'delete',
-    ];
-    $act = isset($_POST['action']) && in_array($_POST['action'], $valid) ? $_POST['action'] : false;
-    if (!$act) {
+    $valid = ['unwarn', 'disable', 'delete'];
+    $act = isset($_POST['action']) && in_array($_POST['action'], $valid, true) ? (string) $_POST['action'] : '';
+    if ($act === '') {
         stderr(_('Error'), _('Something went wrong!'));
     }
-    $cache = $container->get(Cache::class);
- if ($act === 'delete' && has_access($CURUSER['class'], UC_SYSOP, 'coder')) {
-    // TODO: skriv korrekt DELETE query
-    $res_del = $db->perform('/* TODO: delete query */', []);
 
-    if ($res_del) {
-        // success-branch
-    } else {
-        stderr('Error', 'Something went wrong2!');
-    }
-}
-// antag: $db = $container->get(Database::class);
-// antag: $cache, $site_config, $CURUSER, $_uids, $sub er defineret
+    if ($act === 'unwarn') {
+        // Clear leechwarn + reason and notify users
+        $placeholders = implode(',', array_fill(0, count($uids), '?'));
+        $db->run("UPDATE users SET leechwarn = 0, warn_reason = NULL WHERE id IN ($placeholders)", $uids);
 
-if ($act === 'disable') {
-    // Sørg for rene int-UIDs
-    $_uids = array_values(array_unique(array_map('intval', (array) ($_uids ?? []))));
-    if (!$_uids) {
-        stderr('Error', 'No users selected.');
-    }
+        $body = _fe('Hey, your Leech warning was removed by {0}. Please keep in your best behaviour from now on.', $CURUSER['username']);
+        $sub  = _('Leech Warning Removed');
+        $now  = TIME_NOW;
 
-    // Sæt leechwarn = 0 for valgte brugere
-    $ph   = [];
-    $bind = [];
-    foreach ($_uids as $k => $uid) {
-        $ph[] = ":id$k";
-        $bind["id$k"] = $uid;
-    }
-
-    $db->perform(
-        'UPDATE users SET leechwarn = 0 WHERE id IN (' . implode(',', $ph) . ')',
-        $bind
-    );
-
-    // PM-tekst
-    $body = _('Hey, your Leech warning was removed by ') . $CURUSER['username'] . '. '
-          . _('Please keep in your best behaviour from now on.');
-    $now  = (int) TIME_NOW;
-
-    // Send PM til hver bruger + opdatér cache
-    foreach ($_uids as $uid) {
-        $cache->update_row('user_' . $uid, ['leechwarn' => 0], $site_config['expires']['user_cache']);
-
-        $db->perform(
-            'INSERT INTO messages (sender, receiver, subject, msg, added)
-             VALUES (:sender, :receiver, :subject, :msg, :added)',
-            [
-                'sender'   => 2,         // system/bruger-id for afsender
-                'receiver' => $uid,
-                'subject'  => (string) $sub,
-                'msg'      => $body,
+        $buffer = [];
+        foreach ($uids as $uid) {
+            $cache->delete('user_' . (int) $uid);
+            $buffer[] = [
+                'receiver' => (int) $uid,
                 'added'    => $now,
-            ]
-        );
+                'msg'      => $body,
+                'subject'  => $sub,
+            ];
+        }
+        if (!empty($buffer)) {
+            $messages->insert($buffer);
+        }
+
+        header('Refresh: 2; url=' . $r);
+        stderr(_('Success'), _pfe("{0} user's leech warning removed", "{0} users' leech warnings removed", count($uids)));
+    } elseif ($act === 'disable') {
+        // Disable accounts and clear leech warn
+        $reason = _fe('Disabled for leech warning by {0} on {1}', $CURUSER['username'], get_date(TIME_NOW, 'DATE', 1));
+        $params = [$reason];
+        $placeholders = implode(',', array_fill(0, count($uids), '?'));
+        $params = array_merge($params, $uids);
+
+        $db->run("UPDATE users SET status = 2, disable_reason = ?, leechwarn = 0 WHERE id IN ($placeholders)", $params);
+        foreach ($uids as $uid) {
+            $cache->delete('user_' . (int) $uid);
+        }
+
+        header('Refresh: 2; url=' . $r);
+        stderr(_('Success'), _pfe('{0} user disabled', '{0} users disabled', count($uids)));
+    } elseif ($act === 'delete') {
+        if (!has_access((int) $CURUSER['class'], UC_SYSOP, 'coder')) {
+            stderr(_('Error'), _('Permission denied.'));
+        }
+        $placeholders = implode(',', array_fill(0, count($uids), '?'));
+        $db->run("DELETE FROM users WHERE id IN ($placeholders)", $uids);
+        foreach ($uids as $uid) {
+            $cache->delete('user_' . (int) $uid);
+        }
+
+        header('Refresh: 2; url=' . $r);
+        stderr(_('Success'), _pfe('{0} user deleted', '{0} users deleted', count($uids)));
     }
+
+    app_halt('Exit called');
 }
 
-// Rens linkbygning og undgå “&amp;?do=…”
+// -------------------------------------------
+// Views
+// -------------------------------------------
 $base = $site_config['paths']['baseurl'];
-
 switch ($do) {
     case 'disabled':
         $query = "SELECT id, username, class, downloaded, uploaded,
@@ -108,6 +113,7 @@ switch ($do) {
         break;
 
     case 'leechwarn':
+    default:
         $query = "SELECT id, username, class, downloaded, uploaded,
                          IF(downloaded>0, ROUND((uploaded/downloaded),2), '---') AS ratio,
                          warn_reason, leechwarn, registered, last_access
@@ -120,8 +126,9 @@ switch ($do) {
         break;
 }
 
-$g = sql_query($query) or print (is_object($mysqli)) ? mysqli_error($mysqli) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false);
-$count = mysqli_num_rows($g);
+$rows = $db->fetchAll($query);
+$count = count($rows);
+
 $HTMLOUT .= "
     <ul class='level-center bg-06'>
         <li class='is-link margin10'>
@@ -129,7 +136,8 @@ $HTMLOUT .= "
         </li>
     </ul>";
 $HTMLOUT .= "<h2 class='has-text-centered'>" . _('total - ') . " $count " . _(' user') . ' ' . ($count > 1 ? _('s') : '') . '</h2>';
-if ($count == 0) {
+
+if ($count === 0) {
     $HTMLOUT .= stdmsg(_('Hey'), _('There is no ') . strtolower($title));
 } else {
     $HTMLOUT .= "
@@ -144,25 +152,32 @@ if ($count == 0) {
             <th><input type='checkbox' id='checkThemAll'></th>
         </tr>";
     $body = '';
-    while ($a = mysqli_fetch_assoc($g)) {
-        $tip = ($do === 'leechwarn' ? _('Leech Warned for: ') . htmlsafechars($a['warn_reason']) . '<br>' . _(' Warned till ') . get_date((int) $a['leechwarn'], 'DATE', 1) . ' - ' . mkprettytime($a['leechwarn'] - TIME_NOW) : _('Disabled for ') . htmlsafechars($a['disable_reason']));
+    foreach ($rows as $a) {
+        $ratio = is_numeric($a['ratio']) ? (float) $a['ratio'] : '---';
+        $tip = ($do === 'leechwarn'
+            ? _('Leech Warned for: ') . htmlsafechars((string) ($a['warn_reason'] ?? '')) . '<br>'
+              . _(' Warned till ') . get_date((int) $a['leechwarn'], 'DATE', 1) . ' - ' . mkprettytime((int) $a['leechwarn'] - TIME_NOW)
+            : _('Disabled for ') . htmlsafechars((string) ($a['disable_reason'] ?? ''))
+        );
+
         $body .= "
         <tr>
-            <td><a href='userdetails.php?id=" . (int) $a['id'] . "' class='tooltipper' title='$tip'>" . htmlsafechars($a['username']) . '</a></td>
-            <td>' . (float) $a['ratio'] . "<br><span class='small'><b>" . _('D: ') . '</b>' . mksize($a['downloaded']) . '&#160;<b>' . _('U: ') . '</b> ' . mksize($a['uploaded']) . '</span></td>
+            <td><a href='userdetails.php?id=" . (int) $a['id'] . "' class='tooltipper' title='$tip'>" . htmlsafechars((string) $a['username']) . '</a></td>
+            <td>' . $ratio . "<br><span class='small'><b>" . _('D: ') . '</b>' . mksize((int) $a['downloaded']) . '&#160;<b>' . _('U: ') . '</b> ' . mksize((int) $a['uploaded']) . '</span></td>
             <td>' . get_user_class_name((int) $a['class']) . '</td>
             <td>' . get_date((int) $a['last_access'], 'LONG', 0, 1) . '</td>
             <td>' . get_date((int) $a['registered'], 'DATE', 1) . "</td>
             <td><input type='checkbox' name='users[]' value='" . (int) $a['id'] . "'></td>
         </tr>";
     }
+
     $HTMLOUT .= main_table($body, $heading, null, null, 'table-striped', 'checkbox_container');
     $HTMLOUT .= "
         <div class='has-text-centered margin20'>
             <select name='action'>
                 <option value='unwarn'>" . _('Unwarn') . "</option>
                 <option value='disable'>" . _('Disable') . "</option>
-                <option value='delete' " . (!has_access($CURUSER['class'], UC_SYSOP, 'coder') ? 'disabled' : '') . '>' . _('Delete') . "</option>
+                <option value='delete' " . (!has_access((int) $CURUSER['class'], UC_SYSOP, 'coder') ? 'disabled' : '') . '>' . _('Delete') . "</option>
             </select>
                 &raquo;
             <input type='submit' value='" . _('Apply') . "' class='button is-small'>
@@ -170,6 +185,7 @@ if ($count == 0) {
         </div>
     </form>";
 }
+
 $breadcrumbs = [
     "<a href='{$site_config['paths']['baseurl']}/staffpanel.php'>" . _('Staff Panel') . '</a>',
     "<a href='{$_SERVER['PHP_SELF']}'>$title</a>",
