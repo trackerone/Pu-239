86671cad9d2e1029fb6407e1fdc9c709fcd7c21d
trackerone <51250404+trackerone@users.noreply.github.com>
2025-09-09 05:52:57 +0200

    refactor(db): migrate partials to Pu239\Database with bound params

--- partials/categories.php
+++ partials/categories.php
@@ -1,16 +1,15 @@
 <?php
-$db = $container->get(Database::class);
+declare(strict_types=1);
 
 require_once __DIR__ . '/../include/runtime_safe.php';
-
 require_once __DIR__ . '/../include/bootstrap_pdo.php';
 
-
-declare(strict_types = 1);
-
+use Pu239\Database;
 use DI\DependencyException;
 use DI\NotFoundException;
 use Spatie\Image\Exceptions\InvalidManipulation;
+
+$db = $container->get(Database::class);
 
 $user = check_user_status();
 require_once INCL_DIR . 'function_categories.php';
