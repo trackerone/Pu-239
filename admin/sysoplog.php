ff8f22333d1d4a6a05111119dabceb1229d6848a
trackerone <51250404+trackerone@users.noreply.github.com>
2025-09-11 05:52:54 +0200

    migrate admin to pdo

--- admin/sysoplog.php
+++ admin/sysoplog.php
@@ -17,20 +17,30 @@ $db = $container->get(Database::class);
 $class = get_access(basename($_SERVER['REQUEST_URI']));
 class_check($class);
 
-$HTMLOUT = $where = '';
 $search = isset($_POST['search']) ? strip_tags($_POST['search']) : '';
 if (isset($_GET['search'])) {
     $search = strip_tags($_GET['search']);
 }
-if (!empty($search)) {
-    $where = 'WHERE txt LIKE ' . sqlesc("%$search%") . '';
+
+$params = [];
+$where = '';
+if ($search !== '') {
+    $where = 'WHERE txt LIKE :search';
+    $params[':search'] = "%{$search}%";
 }
-//== Delete items older than 1 month
+
+// Delete items older than 1 month
 $secs = 30 * 86400;
-$db->run(');
-$HTMLOUT = '';
-$rows = $db->fetchAll("SELECT added, txt FROM infolog $where ORDER BY added DESC {$pager['limit']}");
-$HTMLOUT .= "
+$cutoff = TIME_NOW - $secs;
+$db->run('DELETE FROM infolog WHERE added < :cutoff', [':cutoff' => $cutoff]);
+
+$count = (int) $db->fetchValue("SELECT COUNT(id) FROM infolog $where", $params);
+$perpage = 30;
+$pager = pager($perpage, $count, "staffpanel.php?tool=sysoplog&amp;action=sysoplog&amp;" . ($search !== '' ? "search=$search&amp;" : ''));
+
+$rows = $db->fetchAll("SELECT added, txt FROM infolog $where ORDER BY added DESC {$pager['limit']}", $params);
+
+$HTMLOUT = "
     <h1 class='has-text-centered'>" . _('Staff actions log') . "</h1>
     <div class='has-text-centered bottom20'>
         <form method='post' action='{$_SERVER['PHP_SELF']}?tool=sysoplog&amp;action=sysoplog' enctype='multipart/form-data' accept-charset='utf-8'>
@@ -38,9 +48,11 @@ $HTMLOUT .= "
             <input type='submit' value='" . _('Search log') . "' class='button is-small'>
         </form>
     </div>";
+
 if ($count > $perpage) {
     $HTMLOUT .= $pager['pagertop'];
 }
+
 if (empty($rows)) {
     $HTMLOUT .= main_div("<div class='padding20'>" . _('No records found') . '</div>');
 } else {
@@ -86,9 +98,11 @@ if (empty($rows)) {
 if ($count > $perpage) {
     $HTMLOUT .= $pager['pagerbottom'];
 }
+
 $title = _('Sysop Log');
 $breadcrumbs = [
     "<a href='{$site_config['paths']['baseurl']}/staffpanel.php'>" . _('Staff Panel') . '</a>',
     "<a href='{$_SERVER['PHP_SELF']}'>$title</a>",
 ];
 echo stdhead($title, [], 'page-wrapper', $breadcrumbs) . wrapper($HTMLOUT) . stdfoot();
+
