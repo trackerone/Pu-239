7195e0ecab49ec09ac64f06d5f4afbf7562ffa07
trackerone <51250404+trackerone@users.noreply.github.com>
2025-09-20 07:54:34 +0200

    refactor: side-effects → return/exception + sanitize (batch offset=0) (#112)

--- include/bittorrent.php
+++ include/bittorrent.php
@@ -2,8 +2,7 @@
 declare(strict_types=1);
 
 if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'] ?? '')) {
-    http_response_code(404);
-    exit(0);
+    throw new \RuntimeException('Direct access to include/bittorrent.php is not permitted.');
 }
 
 use Delight\Auth\Auth;
