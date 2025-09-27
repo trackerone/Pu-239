869f1a2e9f2bf7029cddf486a7f5a52dc138e43c
trackerone <51250404+trackerone@users.noreply.github.com>
2025-09-28 00:13:36 +0200

    feat: CSRF verify + output escaping (batch offset=86) (#187)

--- public/ajax/usersearch.php
+++ public/ajax/usersearch.php
@@ -1,30 +1,17 @@
 <?php
-
 declare(strict_types=1);
 
-<<<<<< codex/enforce-csrf-and-escape-output-hay3lv
-=======
-<<<<<< codex/enforce-csrf-and-escape-output-dxtuor
-=======
-use Pu239\User;
-
->>>>>> master
->>>>>> master
 require_once dirname(__DIR__) . '/bootstrap_web.php';
-require_once __DIR__ . '/../../include/bittorrent.php';
 
-<<<<<< codex/enforce-csrf-and-escape-output-hay3lv
-=======
-<<<<<< codex/enforce-csrf-and-escape-output-dxtuor
->>>>>> master
 use Pu239\User;
+
 require_once __DIR__ . '/../../include/bittorrent.php';
 
 check_user_status();
 
 header('Content-Type: application/json; charset=utf-8');
 
-// TODO(2025): csrf
+// TODO(2025): add CSRF verification
 $keyword = trim((string) ($_POST['keyword'] ?? ''));
 
 if ($keyword === '') {
@@ -34,34 +21,10 @@ if ($keyword === '') {
 
 $users = $container->get(User::class)->search_by_username(strtolower($keyword));
 
-<<<<<< codex/enforce-csrf-and-escape-output-hay3lv
 if (!empty($users)) {
     echo json_encode($users, JSON_THROW_ON_ERROR);
     app_halt('Exit called');
 }
 
-=======
-=======
-check_user_status();
-
-header('Content-Type: application/json; charset=utf-8');
-
-// TODO(2025): csrf
-$keyword = trim((string) ($_POST['keyword'] ?? ''));
-
-if ($keyword === '') {
-    echo json_encode(['data' => _('Invalid Request')], JSON_THROW_ON_ERROR);
-    app_halt('Exit called');
-}
-
-$users = $container->get(User::class)->search_by_username(strtolower($keyword));
-
->>>>>> master
-if (!empty($users)) {
-    echo json_encode($users, JSON_THROW_ON_ERROR);
-    app_halt('Exit called');
-}
-
->>>>>> master
 echo json_encode(['data' => _('Invalid Request')], JSON_THROW_ON_ERROR);
 app_halt('Exit called');
