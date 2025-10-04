777cbf25761eba5e2ae2606ac555d5b0740a776e
trackerone <51250404+trackerone@users.noreply.github.com>
2025-10-04 22:39:41 +0200

    feat(http): front controller + router + middleware (batch offset=140) (#254)

--- classes/Http/Handlers/HomeHandler.php
+++ classes/Http/Handlers/HomeHandler.php
@@ -7,30 +7,17 @@ final class HomeHandler
 {
     public function handle(array $meta = []): void
     {
-        if (isset($meta['legacy']) && is_string($meta['legacy'])) {
-            $legacy = $meta['legacy'];
-            if (is_file($legacy)) {
-                require $legacy;
-                return;
-            }
-        }
-
-        echo 'OK';
-    public function handle(array $meta = []): mixed
-    {
         if (!defined('PU239_ROUTED')) {
             define('PU239_ROUTED', true);
         }
 
-        if (isset($meta['legacy']) && is_string($meta['legacy'])) {
+        if (isset($meta['legacy']) && is_string($meta['legacy']) && is_file($meta['legacy'])) {
             require $meta['legacy'];
+
             return;
         }
 
         echo 'OK';
-        require \dirname(__DIR__, 3) . '/public/index.legacy.php';
-
-        return null;
     }
 }
 
