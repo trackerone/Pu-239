755d2c833d8fdf06086fb9f9fee2ad5c7325c703
trackerone <51250404+trackerone@users.noreply.github.com>
2025-10-05 10:07:01 +0200

    chore(http): reconcile front controller (no-overwrite)

---
+++ classes/Http/Middlewares/ForceHttps.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace PU239\Http\Middlewares;
+
+final class ForceHttps
+{
+    public function process(callable $next): void
+    {
+        $https = $_SERVER['HTTPS'] ?? '';
+        $forwardedProto = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '';
+
+        $isHttps = $https !== '' && strcasecmp((string) $https, 'off') !== 0;
+        $isForwardedHttps = strcasecmp($forwardedProto, 'https') === 0;
+
+        if ($isHttps || $isForwardedHttps) {
+            $next();
+
+            return;
+        }
+
+        $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+        $uri = $_SERVER['REQUEST_URI'] ?? '/';
+
+        header('Location: https://' . $host . $uri, true, 301);
+        exit();
+    }
+}
---
+++ classes/Http/Middlewares/Hsts.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace PU239\Http\Middlewares;
+
+final class Hsts
+{
+    public function __construct(private int $maxAge = 31536000, private bool $includeSubdomains = true)
+    {
+    }
+
+    public function process(callable $next): void
+    {
+        if (!headers_sent()) {
+            $header = 'Strict-Transport-Security: max-age=' . $this->maxAge;
+            if ($this->includeSubdomains) {
+                $header .= '; includeSubDomains';
+            }
+
+            header($header);
+        }
+
+        $next();
+    }
+}
