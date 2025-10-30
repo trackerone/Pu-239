3c6c2094deeb0f63e1349e58d3c453c66f989bdf
trackerone <51250404+trackerone@users.noreply.github.com>
2025-10-30 23:03:48 +0100

    feat(admin): extract admin referrers and usersearch to controllers

--- admin/referrers.php
+++ admin/referrers.php
@@ -1,81 +1,14 @@
 <?php
 declare(strict_types=1);
-require_once dirname(__DIR__) . '/bootstrap_web.php';
 
-use PU239\Config\ConfigRepository;
-use PU239\Security\AuthZ;
-use Pu239\Database;
+use PU239\Admin\Controllers\ReferrersController;
+use Psr\Container\ContainerInterface;
 
-global $container;
-
-AuthZ::requireRole('admin');
-/** @var ConfigRepository $config */
-$config = $container->get(ConfigRepository::class);
-$db = $container->get(Database::class);
-
-$class = get_access(basename($_SERVER['REQUEST_URI']));
-class_check($class);
-
-$HTMLOUT = '';
-$_GET['page'] = !isset($_GET['page']) ? 0 : (int) $_GET['page'];
-$rows = $db->fetchAll('SELECT * FROM referrers');
-$count = mysqli_num_rows($res);
-if ($count > 0) {
-    $HTMLOUT .= "
-    <h1 class='has-text-centered'>" . _('Last referers') . '</h1>';
-    $heading = '
-        <tr>
-            <th>' . _('Nr.') . '</th>
-            <th>' . _('Date / Time') . '</th>
-            <th>' . _('Browser') . '</th>
-            <th>' . _('IP') . '</th>
-            <th>' . _('User') . '</th>
-            <th>' . _('URL') . '</th>
-            <th>' . _('Result') . '</th>
-        </tr>';
-    $perpage = 10;
-    $i = (int) $_GET['page'] * $perpage;
-    $pager = pager($perpage, $count, 'staffpanel.php?tool=referrers&amp;');
-    $rows = $db->fetchAll("SELECT r.*, u.id as uid, u.username FROM referrers AS r LEFT JOIN users AS u ON u.ip = r.ip ORDER BY date DESC {$pager['limit']}");
-    if (mysqli_num_rows($res) > 0) {
-        $body = '';
-        while ($data = mysqli_fetch_assoc($res)) {
-            ++$i;
-            $http_agent = htmlsafechars($data['browser']);
-            if (strstr($http_agent, 'Opera')) {
-                $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/opera.png' alt='Opera' title='Opera' width='25' height='25'>&#160;&#160;Opera";
-            } elseif (strstr($http_agent, 'Konqueror')) {
-                $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/konqueror.png' alt='konqueror' title='konqueror' width='25' height='25'>&#160;&#160;konqueror";
-            } elseif (strstr($http_agent, 'MSIE')) {
-                $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/ie.png' alt='IE' title='IE' width='25' height='25'>&#160;&#160;IE";
-            } elseif (strstr($http_agent, 'Chrome')) {
-                $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/chrome.png' alt='Chrome' title='Chrome' width='25' height='25'>&#160;&#160;Chrome";
-            } elseif ((strstr($http_agent, 'Nav')) || (strstr($http_agent, 'Gold')) || (strstr($http_agent, 'X11')) || (strstr($http_agent, 'Mozilla')) || (strstr($http_agent, 'Netscape'))) {
-                $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/firefox.png' alt='FireFox' title='FireFox' width='25' height='25'>&#160;&#160;Mozilla";
-            } else {
-                $browser = _('Unknow Browser');
-            }
-            $body .= '
-        <tr>
-            <td>' . $i . '</td>
-            <td>' . get_date((int) $data['date'], '') . '</td>
-            <td>' . $browser . '</td>
-            <td>' . htmlsafechars($data['ip']) . '</td>
-            <td>' . htmlsafechars($data['ip']) . ' ' . ((int) $data['uid'] ? format_username((int) $data['uid']) : ' [' . _('Guest') . ']') . "</td>
-            <td><a href='" . htmlsafechars($data['referer']) . "'>" . htmlsafechars(CutName($data['referer'], 50)) . "</a></td>
-            <td><a href='" . htmlsafechars($data['page']) . "'>" . _('page viewed') . '</a></td>
-        </tr>';
-            $browser = '';
-        }
-    }
-    $HTMLOUT .= main_table($body, $heading);
-    $HTMLOUT .= $pager['pagerbottom'];
-} else {
-    $HTMLOUT .= stdmsg(_('Nothing found!'), _('Try again with a refined search string.'));
+/** @var ContainerInterface $container */
+if (!isset($container)) {
+    // TODO(2025): remove fallback once admin bootstrap wires controllers
+    $container = require __DIR__ . '/../bootstrap/admin-container.php';
 }
-$title = _('Referers');
-$breadcrumbs = [
-    "<a href='{$config->get('paths.baseurl')}/staffpanel.php'>" . _('Staff Panel') . '</a>',
-    "<a href='{$_SERVER['PHP_SELF']}'>$title</a>",
-];
-echo stdhead($title, [], 'page-wrapper', $breadcrumbs) . wrapper($HTMLOUT) . stdfoot();
+
+$controller = $container->get(ReferrersController::class);
+$controller([]);
---
+++ classes/Admin/Controllers/ReferrersController.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+namespace PU239\Admin\Controllers;
+
+use PU239\Config\ConfigRepository;
+use PU239\Security\AuthZ;
+use Psr\Container\ContainerInterface;
+use Pu239\Database;
+
+final class ReferrersController
+{
+    public function __construct(
+        private readonly ContainerInterface $container,
+        private readonly ConfigRepository $config,
+        private readonly \PDO $pdo,
+    ) {
+    }
+
+    /** @param array<string,mixed> $meta */
+    public function __invoke(array $meta = []): void
+    {
+        // AUTO_ADMIN_CONVERT: 2025-10-23T00:00:00Z; tool=codex-admin-convert; rules=2025.10.23
+        try {
+            // LEGACY BODY START (from admin/referrers.php)
+            // TODO(2025): wrap referrers report in AdminReportService
+            AuthZ::requireRole('admin');
+            $container = $this->container;
+            /** @var ConfigRepository $config */
+            $config = $this->config;
+
+            $db = $container->get(Database::class);
+
+            $class = get_access(basename($_SERVER['REQUEST_URI']));
+            class_check($class);
+
+            $HTMLOUT = '';
+            $_GET['page'] = !isset($_GET['page']) ? 0 : (int) $_GET['page'];
+            $rows = $db->fetchAll('SELECT * FROM referrers');
+            $count = mysqli_num_rows($res);
+            if ($count > 0) {
+                $HTMLOUT .= "
+    <h1 class='has-text-centered'>" . _('Last referers') . '</h1>';
+                $heading = '
+        <tr>
+            <th>' . _('Nr.') . '</th>
+            <th>' . _('Date / Time') . '</th>
+            <th>' . _('Browser') . '</th>
+            <th>' . _('IP') . '</th>
+            <th>' . _('User') . '</th>
+            <th>' . _('URL') . '</th>
+            <th>' . _('Result') . '</th>
+        </tr>';
+                $perpage = 10;
+                $i = (int) $_GET['page'] * $perpage;
+                $pager = pager($perpage, $count, 'staffpanel.php?tool=referrers&amp;');
+                $rows = $db->fetchAll("SELECT r.*, u.id as uid, u.username FROM referrers AS r LEFT JOIN users AS u ON u.ip = r.ip ORDER BY date DESC {$pager['limit']}");
+                if (mysqli_num_rows($res) > 0) {
+                    $body = '';
+                    while ($data = mysqli_fetch_assoc($res)) {
+                        ++$i;
+                        $http_agent = htmlsafechars($data['browser']);
+                        if (strstr($http_agent, 'Opera')) {
+                            $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/opera.png' alt='Opera' title='Opera' width='25' height='25'>&#160;&#160;Opera";
+                        } elseif (strstr($http_agent, 'Konqueror')) {
+                            $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/konqueror.png' alt='konqueror' title='konqueror' width='25' height='25'>&#160;&#160;konqueror";
+                        } elseif (strstr($http_agent, 'MSIE')) {
+                            $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/ie.png' alt='IE' title='IE' width='25' height='25'>&#160;&#160;IE";
+                        } elseif (strstr($http_agent, 'Chrome')) {
+                            $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/chrome.png' alt='Chrome' title='Chrome' width='25' height='25'>&#160;&#160;Chrome";
+                        } elseif ((strstr($http_agent, 'Nav')) || (strstr($http_agent, 'Gold')) || (strstr($http_agent, 'X11')) || (strstr($http_agent, 'Mozilla')) || (strstr($http_agent, 'Netscape'))) {
+                            $browser = "<img src='{$config->get('paths.images_baseurl')}referrers/firefox.png' alt='FireFox' title='FireFox' width='25' height='25'>&#160;&#160;Mozilla";
+                        } else {
+                            $browser = _('Unknow Browser');
+                        }
+                        $body .= '
+        <tr>
+            <td>' . $i . '</td>
+            <td>' . get_date((int) $data['date'], '') . '</td>
+            <td>' . $browser . '</td>
+            <td>' . htmlsafechars($data['ip']) . '</td>
+            <td>' . htmlsafechars($data['ip']) . ' ' . ((int) $data['uid'] ? format_username((int) $data['uid']) : ' [' . _('Guest') . ']') . "</td>
+            <td><a href='" . htmlsafechars($data['referer']) . "'>" . htmlsafechars(CutName($data['referer'], 50)) . "</a></td>
+            <td><a href='" . htmlsafechars($data['page']) . "'>" . _('page viewed') . '</a></td>
+        </tr>';
+                        $browser = '';
+                    }
+                }
+                $HTMLOUT .= main_table($body, $heading);
+                $HTMLOUT .= $pager['pagerbottom'];
+            } else {
+                $HTMLOUT .= stdmsg(_('Nothing found!'), _('Try again with a refined search string.'));
+            }
+            $title = _('Referers');
+            $breadcrumbs = [
+                "<a href='{$config->get('paths.baseurl')}/staffpanel.php'>" . _('Staff Panel') . '</a>',
+                "<a href='{$_SERVER['PHP_SELF']}'>$title</a>",
+            ];
+            echo stdhead($title, [], 'page-wrapper', $breadcrumbs) . wrapper($HTMLOUT) . stdfoot();
+            // LEGACY BODY END
+        } catch (\Throwable $e) {
+            error_log('Admin controller error (referrers): ' . $e->getMessage());
+            http_response_code(500);
+            echo 'Internal admin error';
+        }
+    }
+}
